2b68e305701fd1200657dff04b1ee99b63536b70
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-08 16:57:04 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var points = 1;
+alert('Wartość to, ' + points); // Wyświetla na ekranie alert
+console.log( 'The current value of points is: ', points ); // Wartość to 1
+console.log( 'The current value of points is: ', points++ ); // Wartość 1, a następnie wzrasta
+console.log( 'The current value of points is: ', points ); // Wartość to 2
