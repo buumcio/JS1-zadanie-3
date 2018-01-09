6804f19cd92edc150c04678aaeeec48bc3e908b9
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-09 12:58:04 +0100

    Change files js - add comment to function alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var points = 1;
-alert('Wartość to, ' + points); // Wyświetla na ekranie alert
+/* alert('Wartość to, ' + points); */
+// funkcja alert została wyłączona
+// poniżej wyświetlimy w konsoli zmienną points
 console.log( 'The current value of points is: ', points ); // Wartość to 1
 console.log( 'The current value of points is: ', points++ ); // Wartość 1, a następnie wzrasta
 console.log( 'The current value of points is: ', points ); // Wartość to 2
